e614afe1a130e92d2e1deb483b5a5f70c794c549
Tobias Mayn <tobiasmayn@gmail.com>
2022-11-22 21:33:50 +0100

    more analysis functions

---
+++ src/document_analysis/DocOperations.java
@@ -0,0 +1,54 @@
+package document_analysis;
+
+public class DocOperations {
+    private Document document = new Document();
+
+    public DocOperations() {
+        System.out.println(getWordCount());
+        System.out.println(getLineCount());
+        System.out.println(getCharacterCountWithSpaces());
+        System.out.println(getCharacterCountNoSpaces());
+
+    }
+
+    public int getWordCount() {
+        String[] words;
+        int wordCount = 0;
+
+        for (String string : document.getDocument()) {
+            words = string.split(" ");
+            wordCount += words.length;
+        }
+
+        return wordCount;
+    }
+
+    public int getLineCount() {
+        return document.getDocument().size();
+    }
+
+    public int getCharacterCountWithSpaces() {
+        int charCount = 0;
+
+        for (String string: document.getDocument()) {
+            charCount += string.chars().count();
+        }
+        
+        return charCount;
+    }
+
+    public int getCharacterCountNoSpaces() {
+        int charCount = 0;
+        String[] words;
+
+        for (String string: document.getDocument()) {
+            words = string.split(" ");
+            for (String word : words) {
+                charCount += word.chars().count();   
+            }
+        }
+        
+        return charCount;
+    }
+
+}
